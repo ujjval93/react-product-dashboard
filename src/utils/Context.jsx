600c32a34aa1968cf06ed57852a2698b5de6cfca
Ujjval Yadav <ujjvalyadav746@gmail.com>
2026-03-06 21:34:56 +0530

    updated context with localStorage

--- src/utils/Context.jsx
+++ src/utils/Context.jsx
@@ -1,10 +1,13 @@
 import React, { createContext, useEffect, useState } from "react";
-import Axios from "./axios";   // 👈 lowercase axios
+import Axios from "./axios";
 
 export const ProductContext = createContext();
 
 const Context = (props) => {
-  const [products, setproduct] = useState([]);
+
+  const [products, setproduct] = useState(
+    JSON.parse(localStorage.getItem("products")) || []
+  );
 
   const getProduct = async () => {
     try {
@@ -16,8 +19,14 @@ const Context = (props) => {
   };
 
   useEffect(() => {
-    getProduct();
+    if(products.length === 0){
+      getProduct();
+    }
   }, []);
+
+  useEffect(() => {
+    localStorage.setItem("products", JSON.stringify(products));
+  }, [products]);
 
   return (
     <ProductContext.Provider value={[products, setproduct]}>
